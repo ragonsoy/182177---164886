f6f1194f1ab372fa9e732946a08358312e0817a0
Camilo-PC\CamiloBadaracco <camilo.bada@gmail.com>
2017-04-29 18:16:13 -0300

    TDD TeamGetNameTest falla prueba.

---
+++ BoardApplication/BoardApplicationTest/TeamUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BoardApplicationBusinessLogic;
+
+namespace BoardApplicationTest
+{
+    [TestClass]
+    public class TeamUnitTest
+    {
+        [TestMethod]
+        public void TeamGetNameTest()
+        {
+            Team team = new Team();
+            Assert.AreEqual(team.getName(), "Nombre");
+        }
+    }
+}
