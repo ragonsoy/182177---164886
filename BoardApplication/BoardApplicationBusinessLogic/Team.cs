e471efd727997082e1e1223e9a94e39802ee0d51
Camilo-PC\CamiloBadaracco <camilo.bada@gmail.com>
2017-04-29 18:19:39 -0300

    TDD TeamGetNameTest prueba superada

---
+++ BoardApplication/BoardApplicationBusinessLogic/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardApplicationBusinessLogic
+{
+    public class Team
+    {
+        public string getName()
+        {
+            return "Nombre";
+        }
+    }
+}
