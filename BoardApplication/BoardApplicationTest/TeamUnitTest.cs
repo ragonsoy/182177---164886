e3b1127110a31c4b166ca4246382dcda3c5c4183
Camilo-PC\CamiloBadaracco <camilo.bada@gmail.com>
2017-04-30 04:06:38 -0300

    TDD TeamSetNameTest prueba falla(no compila)

--- BoardApplication/BoardApplicationTest/TeamUnitTest.cs
+++ BoardApplication/BoardApplicationTest/TeamUnitTest.cs
@@ -10,8 +10,16 @@ namespace BoardApplicationTest
         [TestMethod]
         public void TeamGetNameTest()
         {
-            Team team = new Team();
+            Team team = new Team("Nombre");
             Assert.AreEqual(team.getName(), "Nombre");
+        }
+
+        [TestMethod]
+        public void TeamSetNameTest()
+        {
+            Team team = new Team("NombreACambiar");
+            team.setName("NombreCambiado");
+            Assert.AreEqual(team.getName(), "NombreCambiado");
         }
     }
 }
