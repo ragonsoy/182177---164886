97e568f09adcb76a5b37fc926fd9b46f0777d881
Camilo-PC\CamiloBadaracco <camilo.bada@gmail.com>
2017-05-01 15:54:50 -0300

    TDD UserAdministratorTest

---
+++ BoardApplication/BoardApplicationBusinessLogic/UserAdministrator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardApplicationBusinessLogic
+{
+    public class UserAdministrator : User
+    {
+        
+        public UserAdministrator(string name, string lastName, string email, DateTime birthDate, string password) 
+            : base (name, lastName, email, birthDate, password)
+        {
+        
+        }
+    }
+}
---
+++ BoardApplication/BoardApplicationTest/UserAdminUnitTest.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BoardApplicationBusinessLogic;
+
+namespace BoardApplicationTest
+{
+    [TestClass]
+    public class UserAdminUnitTest
+    {
+        [TestMethod]
+        public void UserAdministratorGetNameTest()
+        {
+            DateTime birthDate = new DateTime();
+            DateTime.TryParse("1/1/2000", out birthDate);
+            User userAdministrator = new UserAdministrator("Nombre", "Apellido", "Email", birthDate, "Password");
+            Assert.AreEqual(userAdministrator.getName(), "Nombre");
+        }
+        
+        [TestMethod]
+        public void UserAdministratorSetNameTest()
+        {
+            DateTime birthDate = new DateTime();
+            DateTime.TryParse("1/1/2000", out birthDate);
+            User userAdministrator = new UserAdministrator("NombreACambiar", "Apellido", "Email", birthDate, "Password");
+            userAdministrator.setName("NombreCambiado");
+            Assert.AreEqual(userAdministrator.getName(), "NombreCambiado");
+        }
+
+        [TestMethod]
+        public void UserAdministratorGetLastNameTest()
+        {
+            DateTime birthDate = new DateTime();
+            DateTime.TryParse("1/1/2000", out birthDate);
+            User userAdministrator = new UserAdministrator("Nombre", "Apellido", "Email", birthDate, "Password");
+            Assert.AreEqual(userAdministrator.getLastName(), "Apellido");
+        }
+
+        [TestMethod]
+        public void UserAdministratorSetLastNameTest()
+        {
+            DateTime birthDate = new DateTime();
+            DateTime.TryParse("1/1/2000", out birthDate);
+            User userAdministrator = new UserAdministrator("Nombrer", "ApellidoACambiar", "Email", birthDate, "Password");
+            userAdministrator.setLastName("ApellidoCambiado");
+            Assert.AreEqual(userAdministrator.getLastName(), "ApellidoCambiado");
+        }
+
+        [TestMethod]
+        public void UserAdministratorGetEmailTest()
+        {
+            DateTime birthDate = new DateTime();
+            DateTime.TryParse("1/1/2000", out birthDate);
+            User userAdministrator = new UserAdministrator("Nombre", "Apellido", "Email", birthDate, "Password");
+            Assert.AreEqual(userAdministrator.getEmail(), "Email");
+        }
+
+        [TestMethod]
+        public void UserAdministratorSetEmailTest()
+        {
+            DateTime birthDate = new DateTime();
+            DateTime.TryParse("1/1/2000", out birthDate);
+            User userAdministrator = new UserAdministrator("Nombrer", "Apellido", "EmailACambiar", birthDate, "Password");
+            userAdministrator.setEmail("EmailCambiado");
+            Assert.AreEqual(userAdministrator.getEmail(), "EmailCambiado");
+        }
+
+        [TestMethod]
+        public void UserAdministratorGetBirthDateTest()
+        {
+            DateTime birthDate = new DateTime();
+            DateTime.TryParse("1/1/2000", out birthDate);
+            User userAdministrator = new UserAdministrator("Nombre", "Apellido", "Email", birthDate, "Password");
+            Assert.AreEqual(userAdministrator.getBirthDate(), birthDate);
+        }
+
+        [TestMethod]
+        public void UserAdministratorSetBirthDateTest()
+        {
+            DateTime birthDate = new DateTime();
+            DateTime.TryParse("1/1/2000", out birthDate);
+            DateTime birthDateAcambiar = new DateTime();
+            DateTime.TryParse("2/2/2000", out birthDateAcambiar);
+            User userAdministrator = new UserAdministrator("Nombrer", "Apellido", "Email", birthDate, "Password");
+            userAdministrator.setBirthDate(birthDateAcambiar);
+            Assert.AreEqual(userAdministrator.getBirthDate(), birthDateAcambiar);
+        }
+
+        [TestMethod]
+        public void UserAdministratorGetPasswordTest()
+        {
+            DateTime birthDate = new DateTime();
+            DateTime.TryParse("1/1/2000", out birthDate);
+            User userAdministrator = new UserAdministrator("Nombre", "Apellido", "Email", birthDate, "Password");
+            Assert.AreEqual(userAdministrator.getPassword(), "Password");
+        }
+
+        [TestMethod]
+        public void UserAdministratorSetPassword()
+        {
+            DateTime birthDate = new DateTime();
+            DateTime.TryParse("1/1/2000", out birthDate);
+            User userAdministrator = new UserAdministrator("Nombrer", "Apellido", "Email", birthDate, "PasswordACambiar");
+            userAdministrator.setPassword("PasswordCambiado");
+            Assert.AreEqual(userAdministrator.getPassword(), "PasswordCambiado");
+        }
+
+    }
+}
