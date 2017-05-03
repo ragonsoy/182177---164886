dd2cab22180c05fdfdaf26a5ddf4bb3bbdfa6925
Gonzalo Silva <ragonsoy@gmail.com>
2017-05-03 13:41:14 -0300

    TDD - CommentaryGetResolutionDateTest & CommentarySetResolutionDateTest & CommentarySetCreationDateTest & CommentaryGetCreationDateTest - Green & refactor -- todavia no resolvi bien si se podria crear un comentario con fecha resolucion NULL, discutor con Camilo.

---
+++ BoardApplication/BoardApplicationBusinessLogic/Commentary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardApplicationBusinessLogic
+{
+    public class Commentary
+    {
+        private DateTime creationDateTime;
+        private DateTime resolutionDateTime;
+
+        public Commentary(DateTime creationDateTime, DateTime resolutionDateTime)
+        {
+            this.creationDateTime = creationDateTime;
+            this.resolutionDateTime = resolutionDateTime;
+        }
+
+        public DateTime getCreationDate()
+        {
+            return this.creationDateTime;
+        }
+
+        public void setCreationDate(DateTime creationDateTime)
+        {
+            this.creationDateTime = creationDateTime;
+
+        }
+
+        public DateTime getResolutionDate()
+        {
+            return this.resolutionDateTime;
+        }
+
+        public void setResolutionDate(DateTime resolutionDateTime)
+        {
+            this.resolutionDateTime = resolutionDateTime;
+        }
+    }
+}
---
+++ BoardApplication/BoardApplicationTest/CommentaryUnitTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BoardApplicationBusinessLogic;
+using System.Collections.Generic;
+
+namespace BoardApplicationTest
+{
+    
+    [TestClass]
+    public class CommentaryUnitTest
+    {
+
+        [TestMethod]
+        public void CommentaryGetCreationDateTest()
+        {
+            DateTime creationDateTime = new DateTime();
+            DateTime.TryParse("1/1/2000", out creationDateTime);
+            DateTime resolutionDateTime = new DateTime();
+            DateTime.TryParse("2/1/2000", out creationDateTime);
+            Commentary commentary = new Commentary(creationDateTime, resolutionDateTime);
+            Assert.AreEqual(commentary.getCreationDate(), creationDateTime);
+        }
+
+        [TestMethod]
+        public void CommentarySetCreationDateTest()
+        {
+            DateTime creationDateTime = new DateTime();
+            DateTime.TryParse("1/1/2000", out creationDateTime);
+            DateTime resolutionDateTime = new DateTime();
+            DateTime.TryParse("2/1/2000", out creationDateTime);
+            Commentary commentary = new Commentary(creationDateTime, resolutionDateTime);
+            DateTime creationDateTimeNew = new DateTime();
+            DateTime.TryParse("2/1/2000", out creationDateTimeNew);
+            commentary.setCreationDate(creationDateTimeNew);
+            Assert.AreEqual(commentary.getCreationDate(), creationDateTimeNew);
+        }
+
+        [TestMethod]
+        public void CommentaryGetResolutionDateTest()
+        {
+            DateTime creationDateTime = new DateTime();
+            DateTime.TryParse("1/1/2000", out creationDateTime);
+            DateTime resolutionDateTime = new DateTime();
+            DateTime.TryParse("2/1/2000", out creationDateTime);
+            Commentary commentary = new Commentary(creationDateTime, resolutionDateTime);
+            Assert.AreEqual(commentary.getResolutionDate(), resolutionDateTime);
+        }
+
+        [TestMethod]
+        public void CommentarySetResolutionDateTest()
+        {
+            DateTime creationDateTime = new DateTime();
+            DateTime.TryParse("1/1/2000", out creationDateTime);
+            DateTime resolutionDateTime = new DateTime();
+            DateTime.TryParse("2/1/2000", out creationDateTime);
+            Commentary commentary = new Commentary(creationDateTime, resolutionDateTime);
+            DateTime resolutionDateTimeNew = new DateTime();
+            DateTime.TryParse("2/2/2000", out resolutionDateTimeNew);
+            commentary.setResolutionDate(resolutionDateTimeNew);
+            Assert.AreEqual(commentary.getResolutionDate(), resolutionDateTimeNew);
+        }
+    }
+}
