05b288a6d3204636690c97b5e50caf220cef86e3
Gonzalo Silva <ragonsoy@gmail.com>
2017-05-03 12:53:22 -0300

    TDD - BoardGetBoardTeamTest & BoardSetBoardTeamTest - Green & Refactor test

--- BoardApplication/BoardApplicationBusinessLogic/board.cs
+++ BoardApplication/BoardApplicationBusinessLogic/board.cs
@@ -9,15 +9,33 @@ namespace BoardApplicationBusinessLogic
     public class Board
     {
         private string name;
+        private Team team;
 
-        public Board(string name)
+        public Board(string name, Team team)
         {
             this.name = name;
+            this.team = team;
         }
 
         public string getName()
         {
             return this.name;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public Team getBoardTeam()
+        {
+            return this.team;
+
+        }
+
+        public void SetBoardTeam(Team boardNewTeam)
+        {
+            this.team = boardNewTeam;
         }
     }
 }
--- BoardApplication/BoardApplicationTest/BoardUnitTest.cs
+++ BoardApplication/BoardApplicationTest/BoardUnitTest.cs
@@ -9,11 +9,57 @@ namespace BoardApplicationTest
     [TestClass]
     public class BoardUnitTest
     {
+
         [TestMethod]
         public void BoardGetNameTest()
         {
-            Board board = new Board("Nombre");
-            Assert.AreEqual(board.getName(), "Nombre");
+            DateTime dateTime = new DateTime();
+            DateTime.TryParse("1/1/2000", out dateTime);
+            int maxUserCount = 1;
+            List<User> teamUsers = new List<User>();
+            Team boardTeam = new Team("NombreTeam", dateTime, "Descripcion", maxUserCount, teamUsers);
+            Board board = new Board("NombreBoard", boardTeam);
+            Assert.AreEqual(board.getName(), "NombreBoard");
         }
+
+        [TestMethod]
+        public void BoardSetNameTest()
+        {
+            DateTime dateTime = new DateTime();
+            DateTime.TryParse("1/1/2000", out dateTime);
+            int maxUserCount = 1;
+            List<User> teamUsers = new List<User>();
+            Team boardTeam = new Team("NombreTeam", dateTime, "Descripcion", maxUserCount, teamUsers);
+            Board board = new Board("NombreBoard", boardTeam);
+            board.setName("NombreCambiado");
+            Assert.AreEqual(board.getName(), "NombreCambiado");
+        }
+
+        [TestMethod]
+        public void BoardGetBoardTeamTest()
+        {
+            DateTime dateTime = new DateTime();
+            DateTime.TryParse("1/1/2000", out dateTime);
+            int maxUserCount = 1;
+            List<User> teamUsers = new List<User>();
+            Team boardTeam = new Team("NombreTeam", dateTime, "Descripcion", maxUserCount, teamUsers);
+            Board board = new Board("NombreBoard", boardTeam);
+            Assert.AreEqual(board.getBoardTeam().getName(), "NombreTeam");
+        }
+
+        [TestMethod]
+        public void BoardSetBoardTeamTest()
+        {
+            DateTime dateTime = new DateTime();
+            DateTime.TryParse("1/1/2000", out dateTime);
+            int maxUserCount = 1;
+            List<User> teamUsers = new List<User>();
+            Team boardTeam = new Team("NombreTeam", dateTime, "Descripcion", maxUserCount, teamUsers);
+            Team boardNewTeam = new Team("NombreNewTeam", dateTime, "Descripcion", maxUserCount, teamUsers);
+            Board board = new Board("NombreBoard", boardTeam);
+            board.SetBoardTeam(boardNewTeam);
+            Assert.AreEqual(board.getBoardTeam().getName(), "NombreNewTeam");
+        }
+
     }
 }
