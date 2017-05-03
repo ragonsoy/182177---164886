9cbd0d3a8b61c960085de0cf9c99301f95faf066
Gonzalo Silva <ragonsoy@gmail.com>
2017-05-03 12:28:59 -0300

    TDD - BoardGetNameTest- red test

---
+++ BoardApplication/BoardApplicationBusinessLogic/board.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardApplicationBusinessLogic
+{
+    public class Board
+    {
+        private string name;
+
+        public Board(string name)
+        {
+            this.name = name;
+        }
+
+        public string getName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BoardApplication/BoardApplicationTest/BoardUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BoardApplicationBusinessLogic;
+using System.Collections.Generic;
+
+namespace BoardApplicationTest
+{
+
+    [TestClass]
+    public class BoardUnitTest
+    {
+        [TestMethod]
+        public void BoardGetNameTest()
+        {
+            Board board = new Board("Nombre");
+            Assert.AreEqual(board.getName(), "Nombre");
+        }
+    }
+}
